b0c2c64cd1003791737c02b3770bdb3a4831a9ca
Matheus de Oliveira <166886779+Maatpz@users.noreply.github.com>
2024-05-01 22:12:47 -0300

    Update AgendaDeCompromissos.c

--- AgendaDeCompromissos.c
+++ AgendaDeCompromissos.c
@@ -50,128 +50,179 @@ int main (void){
 
 
 
-// // Funçao para saber em qual dia da semana inicia o ano 
-// int calcularDiaInicio(int ano) {
-//     int diaInicio = (ano * 365 + (ano - 1) / 4 - (ano - 1) / 100 + (ano) / 400) % 7;
-//     return diaInicio;
-// }
-
-// Funçao teste para o ano bisexto
-// int bisexto(int anoBisexto){
-//     if ((ano % 4 == 0 && ano % 100 !=0) ano || (ano % 400 == 0)){
-//         return 1;    
-//     else{
-//         return 0;
-//     }
-// }
-// int main(){
-//     int anoBisexto;
-//     bisexto(anoBisexto);
-//     // int x = bisexto(anoBisexto)
-//     // return x;
-// }
-
 
 
 // // Funçao q cria um calendario e ja define quantos dias tem no mes. 
-// // Troquei algumas coisas pois vez ou outra estava dando erro na compilação, o ano bisexto esta sendo feito direto pq e um calculo padrao
-// // entao nao deve ter problema, testem o calendario para ver se esta tudo certo. 
+// // Troquei algumas coisas pois vez ou outra estava dando erro na compilação, o ano bisexto esta sendo feito direto pq e um calculo padrao,
+// // entao nao deve ter problema, testem o calendario para ver se esta tudo certo.
+int calendario(int ano, int dia_Inicio);
+// int calendario_Agendamentos(int ano, int dia_Inicio, int agendamento_Dia[], int agendamento_Mes[], int agendamento_Duracao[]);
+int main() {
+    int dia,ano,dia_Inicio,opcao;
+    int agendamento_Dia[31]={0};
+    int agendamento_Mes[12]={0};
+    int agendamento_Duracao[24]={0};
 
-// int Calendario(int ano, int diaInicio) {
-//     int diaAtual;
-//     int diasnoMes[12] = {31, 28 + (ano % 4 == 0 && (ano % 100!= 0 || ano % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    printf("Digite o dia da semana de 1° de Janeiro cai: ");
+    scanf("%d",&dia_Inicio);
+    printf("O ano e bisexto?Informe o ano: ");
+    scanf("%d",&ano);
 
-//     printf("Calendario de %d:\n\n", ano);
+    do{
+        int dia_agen;
+        int mes;
+        int duracao;
+        int contador=0;
+        printf("Menu:\n");
+        printf("1-Mostrar Calendário:\n");
+        printf("2- Inserir Agendamento:\n");
+        printf("3- Mostrar Calendário com agendamentos:\n");
+        printf("4- Sair\n");
+        printf("Qual opção deseja:");
+        scanf("%d",&opcao);
+        switch (opcao){
+            case 1:
+                calendario(ano,dia_Inicio);
+                break;
+            case 2:{
+                
+                printf("Qual dia do agendamento(1 ate 31):");
+                scanf("%d",&dia_agen);
+                printf("Qual o mes do agendamento(1 ate 12):");
+                scanf("%d",&mes);
+                printf("QUal duração do agendamento(horas):");
+                scanf("%d",&duracao);
 
+                for(int i=0;i<31;i++){
+                    if(agendamento_Dia[i]==dia_agen && agendamento_Mes[mes-1]==mes && agendamento_Duracao[0]==duracao){
+                        contador++;
+                    }
+                }
+                if (contador<5){
+                    for (int i=0;i<1;i++){
+                        if(agendamento_Dia[i]==0){
+                            agendamento_Dia[i]=dia_agen;
+                            agendamento_Mes[mes-1]=mes;
+                            agendamento_Duracao[0]=duracao;
+                            printf("Agendamento concluido\n");
+                        }
+                    };
+                } else{
+                        printf("Nao e possivel");
+                    }     
+             
+            };
+             break;  
+            case 3:
+                //nao funciona ideia ruim 
+                // calendario_Agendamentos(ano,dia_Inicio,agendamento_Dia[dia_agen],agendamento_Mes[mes],agendamento_Duracao[duracao]);
+                break;
+            case 4:
+                return 0;
+        };
+    } while (opcao!=4);
+    return 0;
+};
+
+int calendario(int ano, int dia_Inicio){
+    int dia_Atual;
+    int dias_noMes[12] = {31, 28 + (ano % 4 == 0 && (ano % 100!= 0 || ano % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    
+    printf("Calendario de %d:\n\n", ano);
+    for (int mesNum = 0; mesNum < 12; mesNum++){
+        if (mesNum == 0) printf("Janeiro\n");
+        else if (mesNum == 1)
+            printf("Fevereiro\n");
+        else if (mesNum == 2) 
+            printf("Marco\n");
+        else if (mesNum == 3) 
+            printf("Abril\n");
+        else if (mesNum == 4) 
+            printf("Maio\n");
+        else if (mesNum == 5) 
+            printf("Junho\n");
+        else if (mesNum == 6) 
+            printf("Julho\n");
+        else if (mesNum == 7) 
+            printf("Agosto\n");
+        else if (mesNum == 8) 
+            printf("Setembro\n");
+        else if (mesNum == 9) 
+            printf("Outubro\n");
+        else if (mesNum == 10) 
+            printf("Novembro\n");
+        else if (mesNum == 11) 
+            printf("Dezembro\n");
+        
+        printf(" Dom Seg Ter Qua Qui Sex Sab\n");
+        for (int espacos = 0; espacos < dia_Inicio; espacos++){
+            printf("    ");
+        }
+        dia_Atual = 1;
+        while (dia_Atual <= dias_noMes[mesNum]){
+            printf("%4d", dia_Atual);
+            if ((dia_Atual + dia_Inicio) % 7 == 0){
+                printf("\n");
+            }
+            dia_Atual++;
+        }
+        dia_Inicio = (dia_Inicio + dias_noMes[mesNum]) % 7;
+        printf("\n\n");
+    };
+    return 0;
+};
+
+//nao funciona
+// int calendario_Agendamentos(int ano, int dia_Inicio, int agendamento_Dia[], int agendamento_Mes[], int agendamento_Duracao[]) {
+//     int dia_Atual;
+//     int dias_noMes[12] = {31, 28 + (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+//     printf("Calendário de %d com agendamentos:\n\n", ano);
 //     for (int mesNum = 0; mesNum < 12; mesNum++) {
-//         if (mesNum == 0)
-//             printf("Janeiro\n");
-//         else if (mesNum == 1) 
-//             printf("Fevereiro\n");
-//         else if (mesNum == 2) 
-//             printf("Marco\n");
-//         else if (mesNum == 3)
-//             printf("Abril\n");
-//         else if (mesNum == 4) 
-//             printf("Maio\n");
-//         else if (mesNum == 5) 
-//             printf("Junho\n");
-//         else if (mesNum == 6) 
-//             printf("Julho\n");
-//         else if (mesNum == 7) 
-//             printf("Agosto\n");
-//         else if (mesNum == 8) 
-//             printf("Setembro\n");
-//         else if (mesNum == 9) 
-//             printf("Outubro\n");
-//         else if (mesNum == 10) 
-//             printf("Novembro\n");
-//         else if (mesNum == 11) 
-//             printf("Dezembro\n");
-
+//         switch (mesNum + 1) {
+//             case 1: printf("Janeiro\n"); 
+//              break;
+//             case 2: printf("Fevereiro\n");
+//              break;
+//             case 3: printf("Março\n");
+//              break;
+//             case 4: printf("Abril\n");
+//              break;
+//             case 5: printf("Maio\n");
+//              break;
+//             case 6: printf("Junho\n");
+//              break;
+//             case 7: printf("Julho\n");
+//              break;
+//             case 8: printf("Agosto\n");
+//              break;
+//             case 9: printf("Setembro\n");
+//              break;
+//             case 10: printf("Outubro\n");
+//              break;
+//             case 11: printf("Novembro\n");
+//              break;
+//             case 12: printf("Dezembro\n");
+//              break;
+//         }
 //         printf(" Dom Seg Ter Qua Qui Sex Sab\n");
-//         for (int espacos = 0; espacos < diaInicio; espacos++) {
+//         for (int espacos = 0; espacos < dia_Inicio; espacos++) {
 //             printf("    ");
 //         }
-//         diaAtual = 1;
-
-//         while (diaAtual <= diasnoMes[mesNum]) {
-//             printf("%4d", diaAtual);
-//             if ((diaAtual + diaInicio) % 7 == 0) {
+//         dia_Atual = 1;
+//         while (dia_Atual <= dias_noMes[mesNum]) {
+//             printf("%4d", dia_Atual);
+//             for (int i = 0; i < 31; i++) {
+//                 if (agendamento_Dia[i] == dia_Atual && agendamento_Mes[i] == mesNum + 1 && agendamento_Duracao[i] > 0) {
+//                     printf("~"); 
+//                     break;
+//                 }
+//             }
+//             if ((dia_Atual + dia_Inicio) % 7 == 0) {
 //                 printf("\n");
 //             }
-//             diaAtual++;
+//             dia_Atual++;
 //         }
-//         diaInicio = (diaInicio + diasnoMes[mesNum]) % 7;
+//         dia_Inicio = (dia_Inicio + dias_noMes[mesNum]) % 7;
 //         printf("\n\n");
 //     }
-// }
-
-
-
-
-
-
-
-// //  Main para teste do calendario, pode retirar depois
-// int main() {
-//     int ano, diaInicio;
-//     printf("Qual o ano do calendario: ");
-//     scanf("%d", &ano);
-//     diaInicio = calcularDiaInicio(ano);
-//     printf("Em qual dia da semana o ano começa(0 - Domingo, 1 - Segunda-feira..): ");
-//     scanf("%d", &diaInicio);
-//     Calendario(ano, diaInicio);
-// }
-
-
-// int agendamento[365];
-// int main(){
-// int opcao;
-// do {
-//     printf("Menu:\n");
-//     printf("1. Mostrar calendário\n");
-//     printf("2. Inserir agendamento\n");
-//     printf("3. Mostrar calendário com agendamentos\n");
-//     printf("4. Sair\n");
-//     printf("Opção: ");
-//     scanf("%d", &opcao);
-    
-//     switch (opcao) {
-//           case 1:
-//             calendario();
-//             break;
-//           case 2: {
-        
-//             break;
-//         }
-//           case 3:
-            
-//             break;
-//           case 4:
-//                 return 0;
-//             default:
-//                 printf("Opção inválida.\n");
-//         }
-//     } while (opcao != 4);
 // }
