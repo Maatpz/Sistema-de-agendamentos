9c188a442fb195ab39fc85220b473c7625e6092f
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2025-02-22 11:32:04 -0300

    Att

--- calendario.c
+++ calendario.c
@@ -1,194 +1,192 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int AgendamentosPorDia[366] = {0};
-int diaAgendamento = 0; int mesAgendamento = 0; int anoAgendamento = 0;
+#define MAX_DIAS 366
+
+int AgendamentosPorDia[MAX_DIAS] = {0};
+
+ void limparTela() {
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+        }
+        void desenharLinha(int largura) {
+            for (int i = 0; i < largura; i++) {
+                printf("-");
+            }
+            printf("\n");
+        }
+        void imprimirCentralizado(const char* texto, int largura) {
+            int espacos = (largura - strlen(texto)) / 2;
+            for (int i = 0; i < espacos; i++) {
+                printf(" ");
+            }
+            printf("%s\n", texto);
+        }
+        void exibirCabecalho(const char* titulo) {
+            limparTela();
+            desenharLinha(60);
+            imprimirCentralizado(titulo, 60);
+            desenharLinha(60);
+        }
+
 
 int calendario(int ano, int diaInicio);
 int calendarioComAgendamentos(int ano, int diaInicio);
 int inserirAgendamento(int diaAgendamento, int mesAgendamento, int anoAgendamento, int diasTratamento);
 int bisexto(int y);
 
-int main(void){
+int main(void) {
     int diaSemana = 0;
-    char anoBi;
     int diasTratamento = 0;
+    int anoAgendamento = 0;
+    int diaAgendamento = 0;
+    int mesAgendamento = 0;
 
-    // Dia semana e ano bissexto passados pelo usuário
-    printf("Informe o 1º dia da semana de janeiro do ano atual! Digite 1-Domingo, 2-Segunda, 3-Terça: \n");
-    scanf("%d", &diaSemana);
-    printf("O ano atual é bissexto? Digite 's' para sim ou 'n' para não: ");
-    scanf(" %c", &anoBi);
+    do {
+        printf("Informe o 1º dia da semana de janeiro do ano atual! Digite 1-Domingo, 2-Segunda, 3-Terça, 4-Quarta, 5-Quinta, 6-Sexta, 7-Sábado: \n");
+        scanf("%d", &diaSemana);
+    } while (diaSemana < 1 || diaSemana > 7);
 
-    // Verificar se o ano é bissexto
-    if (anoBi == 's'){
-        int anoAgendamento[366] = {0};
-    }
-    else{
-        int anoAgendamento[365] = {0};
-    }
+    printf("Digite o ano atual no formato YYYY: ");
+    scanf("%d", &anoAgendamento);
     
-    //loop em do-while do menu
     int opcao;
-    do{
-        printf("\n******Menu******\n");
+    do {
+        exibirCabecalho("Menu Principal");
         printf("1. Mostrar calendário\n");
         printf("2. Inserir agendamento\n");
         printf("3. Mostrar calendário com agendamentos\n");
         printf("4. Sair\n");
+        desenharLinha(60);
         printf("Opção: ");
         scanf("%d", &opcao);
 
-        switch (opcao){
-        case 1:
-            calendario(anoAgendamento, diaSemana);
+        switch (opcao) {
+            case 1:
+                exibirCabecalho("Calendário");
+                calendario(anoAgendamento, diaSemana);
+                break;
+            case 2:
+                exibirCabecalho("Inserir Agendamento");
+                printf("Digite o ano do agendamento no formato YYYY: ");
+                scanf("%d", &anoAgendamento);
+                printf("Digite o mês do agendamento no formato MM: ");
+                scanf("%d", &mesAgendamento);
+                printf("Digite o dia do agendamento no formato DD: ");
+                scanf("%d", &diaAgendamento);
+                printf("Digite a duração do tratamento em dias: ");
+                scanf("%d", &diasTratamento);
+                inserirAgendamento(diaAgendamento, mesAgendamento, anoAgendamento, diasTratamento);
+                break;
+            case 3:
+                exibirCabecalho("Calendário com Agendamentos");
+                calendarioComAgendamentos(anoAgendamento, diaSemana);
             break;
-        case 2:
-            printf("Digite o ano do agendamento no formato YYYY: ");
-            scanf("%d", &anoAgendamento);
-            printf("Digite o mês do agendamento no formato MM: ");
-            scanf("%d", &mesAgendamento);
-            printf("Digite o dia do agendamento no formato DD: ");
-            scanf("%d", &diaAgendamento);
-            printf("Digite a duração do tratamento em dias: ");
-            scanf("%d", &diasTratamento);
-            inserirAgendamento(diaAgendamento, mesAgendamento, anoAgendamento, diasTratamento);
-            break;
-        case 3:
-            calendarioComAgendamentos(2024, 1);
-            break;
-        case 4:
-            printf("Obrigado\n");
-            break;
+            case 4:
+                exibirCabecalho("Saindo");
+                printf("Obrigado por usar o Sistema de Agendamentos!\n");
+                break;
+            default:
+                exibirCabecalho("Erro");
+                printf("Opção inválida. Por favor, tente novamente.\n");
+        }
+        if (opcao != 4) {
+            printf("\nPressione Enter para continuar");
+            getchar(); 
+            getchar();
         }
     } while (opcao != 4);
+
+    return 0;
 }
 
-// Função para exibir o calendario!
-int calendario(int ano, int diaInicio){
-    int diaAtual;
-    int diasnoMes[12] = {31, 28 + (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    printf("Calendario de %d:\n\n", anoAgendamento);
-
-    for (int mesNum = 0; mesNum < 12; mesNum++){
-        if (mesNum == 0)
-            printf("Janeiro\n");
-        else if (mesNum == 1)
-            printf("Fevereiro\n");
-        else if (mesNum == 2)
-            printf("Março\n");
-        else if (mesNum == 3)
-            printf("Abril\n");
-        else if (mesNum == 4)
-            printf("Maio\n");
-        else if (mesNum == 5)
-            printf("Junho\n");
-        else if (mesNum == 6)
-            printf("Julho\n");
-        else if (mesNum == 7)
-            printf("Agosto\n");
-        else if (mesNum == 8)
-            printf("Setembro\n");
-        else if (mesNum == 9)
-            printf("Outubro\n");
-        else if (mesNum == 10)
-            printf("Novembro\n");
-        else if (mesNum == 11)
-            printf("Dezembro\n");
-
-        printf(" Dom Seg Ter Qua Qui Sex Sab\n");
-        for (int espacos = 0; espacos < diaInicio; espacos++){
-            printf("    ");
-        }
-        diaAtual = 1;
-
-        while (diaAtual <= diasnoMes[mesNum]){
-            printf("%4d", diaAtual);
-            if ((diaAtual + diaInicio) % 7 == 0){
-                printf("\n");
-            }
-            diaAtual++;
-        }
-        diaInicio = (diaInicio + diasnoMes[mesNum]) % 7;
-        printf("\n\n");
-    }
-};
-
-// Calendario com o agendamento!
-int calendarioComAgendamentos(int ano, int diaInicio){
-    int diaAtual;
-    int diasnoMes[12] = {31, 28 + (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int calendario(int ano, int diaInicio) {
+    const char *meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
+                           "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+    int diasnoMes[] = {31, 28 + bisexto(ano), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     printf("Calendario de %d:\n\n", ano);
-    int numeroDoDiaNoAno = 1;
 
-    for (int mesNum = 0; mesNum < 12; mesNum++){
-        if (mesNum == 0)
-            printf("Janeiro\n");
-        else if (mesNum == 1)
-            printf("Fevereiro\n");
-        else if (mesNum == 2)
-            printf("Março\n");
-        else if (mesNum == 3)
-            printf("Abril\n");
-        else if (mesNum == 4)
-            printf("Maio\n");
-        else if (mesNum == 5)
-            printf("Junho\n");
-        else if (mesNum == 6)
-            printf("Julho\n");
-        else if (mesNum == 7)
-            printf("Agosto\n");
-        else if (mesNum == 8)
-            printf("Setembro\n");
-        else if (mesNum == 9)
-            printf("Outubro\n");
-        else if (mesNum == 10)
-            printf("Novembro\n");
-        else if (mesNum == 11)
-            printf("Dezembro\n");
-
+    for (int mesNum = 0; mesNum < 12; mesNum++) {
+        printf("%s\n", meses[mesNum]);
         printf(" Dom Seg Ter Qua Qui Sex Sab\n");
-        for (int espacos = 0; espacos < diaInicio; espacos++){
+        
+        for (int espacos = 0; espacos < diaInicio; espacos++) {
             printf("    ");
         }
-        diaAtual = 1;
-
-        while (diaAtual <= diasnoMes[mesNum]){
-            printf("%4d", AgendamentosPorDia[numeroDoDiaNoAno-1]);
-            if ((diaAtual + diaInicio) % 7 == 0){
+        
+        for (int diaAtual = 1; diaAtual <= diasnoMes[mesNum]; diaAtual++) {
+            printf("%4d", diaAtual);
+            if ((diaAtual + diaInicio) % 7 == 0 || diaAtual == diasnoMes[mesNum]) {
                 printf("\n");
             }
-            diaAtual++;
+        }
+        
+        diaInicio = (diaInicio + diasnoMes[mesNum]) % 7;
+        printf("\n");
+    }
+    return 0;
+}
+
+int calendarioComAgendamentos(int ano, int diaInicio) {
+    const char *meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
+                           "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+    int diasnoMes[] = {31, 28 + bisexto(ano), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    printf("Calendario de %d com agendamentos:\n\n", ano);
+    int numeroDoDiaNoAno = 1;
+
+    for (int mesNum = 0; mesNum < 12; mesNum++) {
+        printf("%s\n", meses[mesNum]);
+        printf(" Dom Seg Ter Qua Qui Sex Sab\n");
+        
+        for (int espacos = 0; espacos < diaInicio; espacos++) {
+            printf("    ");
+        }
+        
+        for (int diaAtual = 1; diaAtual <= diasnoMes[mesNum]; diaAtual++) {
+            printf("%4d", AgendamentosPorDia[numeroDoDiaNoAno-1]);
+            if ((diaAtual + diaInicio) % 7 == 0 || diaAtual == diasnoMes[mesNum]) {
+                printf("\n");
+            }
             numeroDoDiaNoAno++;
         }
+        
         diaInicio = (diaInicio + diasnoMes[mesNum]) % 7;
-        printf("\n\n");
-    }   
-};
+        printf("\n");
+    }
+    return 0;
+}
 
-//funçao para verificar se o ano e bisexto
-int bisexto(int y){
+int bisexto(int y) {
     return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
-};
+}
 
-//Função para adicionar agendamentos
-int inserirAgendamento(int diaAgendamento, int mesAgendamento, int anoAgendamento, int diasTratamento){
-    int mo[12] = {31,28+bisexto(anoAgendamento),31,30,31,30,31,31,30,31,30,31};
-    int i;
-    int numeroDiaDoAno=0;
+int inserirAgendamento(int diaAgendamento, int mesAgendamento, int anoAgendamento, int diasTratamento) {
+    int mo[] = {31,28+bisexto(anoAgendamento),31,30,31,30,31,31,30,31,30,31};
+    int numeroDiaDoAno = 0;
 
-    for (i=0; i<(mesAgendamento-1); i++) {
+    for (int i = 0; i < (mesAgendamento-1); i++) {
         numeroDiaDoAno += mo[i];
     }
 
     numeroDiaDoAno += diaAgendamento;
 
-    for (int i=0; i< diasTratamento; i++) {
-        if ((AgendamentosPorDia[numeroDiaDoAno - 1+i]) < 5) {
-            AgendamentosPorDia[numeroDiaDoAno - 1+i]= AgendamentosPorDia[numeroDiaDoAno - 1 +i] + 1;
+    for (int i = 0; i < diasTratamento; i++) {
+        if (numeroDiaDoAno - 1 + i >= MAX_DIAS) {
+            printf("Erro: Tentativa de agendar além do limite do ano.\n");
+            return 1;
+        }
+        if (AgendamentosPorDia[numeroDiaDoAno - 1 + i] < 5) {
+            AgendamentosPorDia[numeroDiaDoAno - 1 + i]++;
         } else {
-            printf("%s%d%s%d%s","Não foi possível agendar seu atendimento para o dia ", diaAgendamento+i, "/", mesAgendamento,  " por conflito na Agenda. Por favor escolha outra data.\n\n");
+            printf("Não foi possível agendar seu atendimento para o dia %d/%d por conflito na Agenda. Por favor escolha outra data.\n\n", 
+                   diaAgendamento + i, mesAgendamento);
         }
     }
-};
+    return 0;
+}
